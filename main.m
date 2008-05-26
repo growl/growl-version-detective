9f557cafd052a069a966203e74b30562529d87c5
Peter Hosey <prh@boredzo.org>
2008-05-26 13:45:02 -0700

    Initial check-in. Known bug: The query results table doesn't update as results come in.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Growl Version Detective
+//
+//  Created by Peter Hosey on 2008-05-26.
+//  Copyright Peter Hosey 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
